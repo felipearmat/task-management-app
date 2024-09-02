26c887dbfafa76f3d4b78c780e2b95fcc3ee9ad3
Felipe A Matos <felipe.matos@leasequery.com>
2024-09-02 17:29:22 -0300

    fix: Added and configured sidekiq testing to rspec

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -7,6 +7,7 @@ abort("The Rails environment is running in production mode!") if Rails.env.produ
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
 
+require 'sidekiq/testing'
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
@@ -29,6 +30,9 @@ begin
 rescue ActiveRecord::PendingMigrationError => e
   abort e.to_s.strip
 end
+
+Sidekiq::Testing.inline!
+
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = Rails.root.join('spec/fixtures')
@@ -64,4 +68,13 @@ RSpec.configure do |config|
   config.include FactoryBot::Syntax::Methods
   config.include Devise::Test::ControllerHelpers, type: :controller
   config.include Devise::Test::IntegrationHelpers, type: :request
+
+  config.before(:each) do
+    ActiveJob::Base.queue_adapter = :test
+    Sidekiq::Worker.clear_all
+  end
+
+  config.after(:each) do
+    Sidekiq::Worker.clear_all
+  end
 end
